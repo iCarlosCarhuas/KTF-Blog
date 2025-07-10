b6512b8eb07ab6b9d4cd5adbde93cf89bd14f6f7
iCarlosCarhuas <CRCT-2003@hotmail.com>
2025-07-10 13:13:42 -0500

    KTF Blog

---
+++ src/main/java/com/app/ktf/blog/KtfApplication.java
@@ -0,0 +1,13 @@
+package com.app.ktf.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtfApplication.class, args);
+	}
+
+}
